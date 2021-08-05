d2cb9b541347fa26acbc63f4330494115ab4153b
zzkzzkjsw <zzk5678@163.com>
2021-08-06 00:47:42 +0800

    master

--- test.py
+++ test.py
@@ -4,4 +4,6 @@
 1. 命令行中输入 python -m venv .venv
 2. 在vscode的左下角更改python的环境（解释器）
 3. 在vscode中新建powershell，在这个新的命令行中pip install
+
+https://code.visualstudio.com/docs/python/environments#_create-a-virtual-environment
 '''
